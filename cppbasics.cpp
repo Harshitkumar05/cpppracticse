102c10abd177a71f1f4a9924c72190b62c4c89ed
Harshit Kumar <115911046+Harshitkumar05@users.noreply.github.com>
2025-01-30 11:51:16 +0530

    Add files via upload

---
+++ cppbasics.cpp
@@ -0,0 +1,14 @@
+//Try these beginner programs to get comfortable with basic syntax:
+
+// ✔ Print "Hello, World!"
+// ✔ Take user input for name and print "Hello, [name]!"
+// ✔ Write a program that swaps two numbers without using a third variable
+// ✔ Find the sum and product of two numbers entered by the user
+//1.
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World!";
+}
+
---
+++ cppq4.cpp
@@ -0,0 +1,13 @@
+//✔ Find the sum and product of two numbers entered by the user
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of a and b "<<endl;
+    cin>>a>>b;
+    int sum=a+b;
+    int product=a*b;
+    cout<<"The sum is :"<<sum<<"and product is:"<<product;
+
+
+}
